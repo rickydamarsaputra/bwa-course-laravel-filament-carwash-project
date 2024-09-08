c153b3380c29ed128f466a6eb9b26be221b0abfe
rickydamarsaputra <retrocode.rc@gmail.com>
2024-09-08 10:59:44 +0700

    Bikin ORM relationship tiap model

--- app/Models/CarService.php
+++ app/Models/CarService.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class CarService extends Model
 {
@@ -20,4 +22,15 @@ class CarService extends Model
         'photo',
         'duration_in_hour'
     ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function storeServices(): HasMany
+    {
+        return $this->hasMany(StoreService::class, 'car_service_id');
+    }
 }
--- app/Models/City.php
+++ app/Models/City.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class City extends Model
 {
@@ -16,4 +18,15 @@ class City extends Model
         'name',
         'slug'
     ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function stores(): HasMany
+    {
+        return $this->hasMany(CarStore::class);
+    }
 }
