34e9b87077d2670c32a55f7d55774d547b2bf067
rickydamarsaputra <retrocode.rc@gmail.com>
2024-09-08 20:32:12 +0700

    Integrasi halaman home

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CarService;
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
-    //
+    public function index()
+    {
+        $cities = City::all();
+        $services = CarService::with(['storeServices'])->get();
+
+        return view('front.index', compact('cities', 'services'));
+    }
 }
